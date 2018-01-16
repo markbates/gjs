76a6054d718a96ad622956da83ae2dc572d6b251
Mark Bates <mark@markbates.com>
2018-01-16 12:50:25 -0500

    plush works

--- assets/js/main.go
+++ assets/js/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "github.com/gopherjs/gopherjs/js"
+import (
+	"strings"
+
+	"github.com/gobuffalo/plush"
+	"github.com/gopherjs/gopherjs/js"
+	"github.com/pkg/errors"
+)
 
 func main() {
-	js.Global.Get("document").Call("write", "Hello world!")
+	ctx := plush.NewContext()
+	ctx.Set("name", "Ringo")
+	ctx.Set("mCall", func(help plush.HelperContext) (string, error) {
+		n, ok := help.Value("name").(string)
+		if !ok {
+			return "", errors.Errorf("expected name to be a string, got %T", help.Value("name"))
+		}
+		return strings.ToUpper(n), nil
+	})
+	s, err := plush.Render(t, ctx)
+	if err != nil {
+		panic(err)
+	}
+	js.Global.Get("document").Call("write", s)
 }
+
+const t = `
+Hello <%= name %>
+
+<%= mCall() %>
+`
